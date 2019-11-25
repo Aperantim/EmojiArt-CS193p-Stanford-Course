dcace555b8b2c34ddc2d7ac75dffe0e9f1c82e0d
Vitaliy Nesterov <liyuop@mail.ru>
2019-11-26 01:32:24 +0300

    3rd commit

---
+++ EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Виталий on 20.11.2019.
+//  Copyright © 2019 Aperantim. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
